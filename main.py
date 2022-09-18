a38e611ae5d51ecc91cff2bf851b39c1766dfb04
Kamaal <kamaal.f1@gmail.com>
2022-09-18 21:57:20 +0200

    Refactoring to get all lines

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import subprocess
 from typing import Optional
+from dataclasses import dataclass
 
 SWIFTLINT_APP = "/usr/local/bin/swiftlint"
 
 
 def main():
-    process = subprocess.Popen(f"{SWIFTLINT_APP}", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output = process.stdout.readlines()
-    if len(output) < 1:
-        raise Exception("No output from swiftlint")
+    output = subprocess.getoutput(f"{SWIFTLINT_APP}")
 
-    result = output[-1].decode("utf-8").strip()
-    swiftlint_result = SwiftlintResult(result)
+    result = output.splitlines()[-1].strip()
+    swiftlint_result = SwiftlintResult.from_swiftlint_result(result=result)
 
     if swiftlint_result.serious is None:
         raise Exception("Could not parse serious count")
@@ -19,14 +17,18 @@ def main():
     if swiftlint_result.serious > 0:
         raise Exception(f"{swiftlint_result.serious} serious issues found")
 
-    print(f"Swiftlint found no serious issues and found {swiftlint_result.violations} violations")
+    print(
+        f"Swiftlint found no serious issues and found {swiftlint_result.violations} violations"
+    )
 
 
-class SwiftlintResult(object):
+@dataclass
+class SwiftlintResult:
     violations: Optional[int] = None
     serious: Optional[int] = None
 
-    def __init__(self, result: str):
+    @staticmethod
+    def from_swiftlint_result(result: str):
         splitted_result = result.split(" ")
         if len(splitted_result) < 6:
             return
@@ -34,10 +36,14 @@ class SwiftlintResult(object):
         violations = splitted_result[3]
         serious = splitted_result[5]
         try:
-            self.violations = int(violations)
-            self.serious = int(serious)
+            amount_of_violation_warnings = int(violations)
+            amount_of_serious_errors = int(serious)
         except (ValueError, TypeError):
             return
+
+        return SwiftlintResult(
+            violations=amount_of_violation_warnings, serious=amount_of_serious_errors
+        )
 
 
 main()
